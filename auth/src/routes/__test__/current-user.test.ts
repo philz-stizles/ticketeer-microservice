366d14c3714fbf972e687917d22d2487fa70f324
philz-stizles <theophilusighalo@gmail.com>
2021-08-27 03:03:42 +0100

    beans

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -1,22 +1,24 @@
-import request from 'supertest';
-import app from '../../app';
+import request from 'supertest'
+import app from '../../app'
+import { signin } from '../../test/setup'
 
 describe('Current user route', () => {
   it('responds with details about the current user', async () => {
-    const cookie = await global.signin();
+    const cookie = await signin()
+    // const cookie = await global.signin()
 
     const response = await request(app)
       .get('/api/users/currentuser')
       .set('Cookie', cookie)
       .send()
-      .expect(200);
+      .expect(200)
 
-    expect(response.body.currentUser.email).toEqual('test@test.com');
-  });
+    expect(response.body.currentUser.email).toEqual('test@test.com')
+  })
 
   it('responds with null if not authenticated', async () => {
-    const response = await request(app).get('/api/users/currentuser').send();
+    const response = await request(app).get('/api/users/currentuser').send()
 
-    expect(response.body.currentUser).toEqual(null);
-  });
-});
+    expect(response.body.currentUser).toEqual(null)
+  })
+})
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,46 +1,46 @@
-import { MongoMemoryServer } from 'mongodb-memory-server';
-import mongoose from 'mongoose';
-import request from 'supertest';
-import app from '../app';
+import { MongoMemoryServer } from 'mongodb-memory-server'
+import mongoose from 'mongoose'
+import request from 'supertest'
+import app from '../app'
 
 declare global {
   namespace NodeJS {
     interface Global {
-      signin(): Promise<string[]>;
+      signin(): Promise<string[]>
     }
   }
 }
 
-let mongo: any;
+let mongo: any
 
 beforeAll(async () => {
-  process.env.JWT_KEY = 'nh7dd4456vhbgkjvdDWQZXMKOY6FDEAmkbZMOPI';
+  process.env.JWT_KEY = 'nh7dd4456vhbgkjvdDWQZXMKOY6FDEAmkbZMOPI'
 
-  mongo = await MongoMemoryServer.create();
-  const mongoUri = mongo.getUri();
+  mongo = await MongoMemoryServer.create()
+  const mongoUri = mongo.getUri()
 
   await mongoose.connect(mongoUri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  });
-});
+  })
+})
 
 beforeEach(async () => {
-  const collections = await mongoose.connection.db.collections();
+  const collections = await mongoose.connection.db.collections()
 
   for (let collection of collections) {
-    await collection.deleteMany({});
+    await collection.deleteMany({})
   }
-});
+})
 
 afterAll(async () => {
-  await mongo.stop();
-  await mongoose.connection.close();
-});
+  await mongo.stop()
+  await mongoose.connection.close()
+})
 
-global.signin = async () => {
-  const email = 'test@test.com';
-  const password = 'password';
+export const signin = async () => {
+  const email = 'test@test.com'
+  const password = 'password'
 
   const response = await request(app)
     .post('/api/users/signup')
@@ -48,8 +48,24 @@ global.signin = async () => {
       email,
       password,
     })
-    .expect(201);
+    .expect(201)
 
-  const cookie = response.get('Set-Cookie');
-  return cookie;
-};
+  const cookie = response.get('Set-Cookie')
+  return cookie
+}
+
+// global.signin = async () => {
+//   const email = 'test@test.com'
+//   const password = 'password'
+
+//   const response = await request(app)
+//     .post('/api/users/signup')
+//     .send({
+//       email,
+//       password,
+//     })
+//     .expect(201)
+
+//   const cookie = response.get('Set-Cookie')
+//   return cookie
+// }
